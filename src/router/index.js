28aea704057e5d2a1df73432ae9ba45d1950eb07
jiangchunming <jiangchunming@emay.cn>
2018-08-27 10:17:18 +0800

    router history

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,63 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-// import Home from '@/components/home/index-home.vue'
-// import Detail from '@/components/detail/index-detail.vue'
-// import NotFound from '@/components/not-found/index-not-found.vue'
-// import Enter from '@/components/login/index-login.vue'
-// import User from '@/components/user/index-user.vue'
-// import Company from '@/components/company/index-company.vue'
+import Home from '@/components/home/index-home.vue'
+import Detail from '@/components/detail/index-detail.vue'
+import NotFound from '@/components/not-found/index-not-found.vue'
+import Enter from '@/components/login/index-login.vue'
+import User from '@/components/user/index-user.vue'
+import Company from '@/components/company/index-company.vue'
 
 Vue.use(Router)
+// 懒加载配置
+// export default new Router({
+//   // mode: 'history',
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld,
+//       children: [
+//         {
+//           path: '/',
+//           component: resolve => require(['@/components/home/index-home.vue'], resolve)
+//         },
+//         {
+//           path: '/detail/:id',
+//           component: resolve => require(['@/components/detail/index-detail.vue'], resolve)
+//         },
+//         {
+//           path: '/login',
+//           component: resolve => require(['@/components/login/index-login.vue'], resolve)
+//         },
+//         {
+//           path: '/user',
+//           component: resolve => require(['@/components/user/index-user.vue'], resolve)
+//         },
+//         {
+//           path: '/company',
+//           component: resolve => require(['@/components/company/index-company.vue'], resolve)
+//         },
+//         {
+//           path: '/index',
+//           redirect: '/'
+//         },
+//         {
+//           path: '*',
+//           component: resolve => require(['@/components/not-found/index-not-found.vue'], resolve)
+//         }
+//       ]
+//     },
+//     {
+//       path: '/index',
+//       redirect: '/'
+//     }
+//   ]
+// })
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
+  base: '/index/index',
   routes: [
     {
       path: '/',
@@ -20,23 +66,23 @@ export default new Router({
       children: [
         {
           path: '/',
-          component: resolve => require(['@/components/home/index-home.vue'], resolve)
+          component: Home
         },
         {
           path: '/detail/:id',
-          component: resolve => require(['@/components/detail/index-detail.vue'], resolve)
+          component: Detail
         },
         {
           path: '/login',
-          component: resolve => require(['@/components/login/index-login.vue'], resolve)
+          component: Enter
         },
         {
           path: '/user',
-          component: resolve => require(['@/components/user/index-user.vue'], resolve)
+          component: User
         },
         {
           path: '/company',
-          component: resolve => require(['@/components/company/index-company.vue'], resolve)
+          component: Company
         },
         {
           path: '/index',
@@ -44,7 +90,7 @@ export default new Router({
         },
         {
           path: '*',
-          component: resolve => require(['@/components/not-found/index-not-found.vue'], resolve)
+          component: NotFound
         }
       ]
     },
